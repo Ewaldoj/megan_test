4dc299bf348eb187f3c3533b6c55e037708eb02e
Ewaldo Leitao <ewaldoj@gmail.com>
2024-09-23 16:58:43 -0400

    add plot

--- test.R
+++ test.R
@@ -2,5 +2,11 @@ library(usethis)
 library(tidyverse)
 
 #Comment test
-x <- 15
-y <- 17
+x <- rnorm(100, mean = 50, sd = 2)
+y = c(1:100)
+
+df = data.frame(x,y)
+
+ggplot(data= df, aes(x, y))+
+  geom_point()
+
